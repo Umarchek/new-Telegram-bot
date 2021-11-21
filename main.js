18f12abda32da8da8faa45458b08708310a1ab45
UZBase <sharofiddinov.07@mail.ru>
2021-11-21 17:57:34 +0300

    123

--- main.js
+++ main.js
@@ -1,86 +1,136 @@
-const TelegramBot = require('node-telegram-bot-api'); // подключаем node-telegram-bot-api
-const token = '2102216686:AAF4Y3hwMGcBoAARfvhNscI7Fh_Gg0O3vLE';
-const bot = new TelegramBot(token, {polling: true});// создаем бота
 
-const getData = require('./getData.js'); // обслуживаем API fetch
-// сам бот::
-const keyboard = require('./keyboard.js'); // описываем меню бота
+/* Get you Telegram Bot Token @BotFater */
 
-// Обрабтка отправки картинки
-const sendImg = (chatId,img) =>{
-    if (img) {
-        bot.sendPhoto(chatId, img);
-    } 
-}
-// Обработка валюты 
-const sendMoneyExchengeRates = (chatId,money) =>{
- 
-    getData('https://api.exchangeratesapi.io/latest?base=USD').then( data  => {
-            const rate = data.rates;                       
-            bot.sendMessage(chatId, ` USD = ${rate[money].toFixed(2)}`);      
-            
+const TOKEN = '2102216686:AAF4Y3hwMGcBoAARfvhNscI7Fh_Gg0O3vLE';
+var TelegramBot = require('node-telegram-bot-api');
+var bot = new TelegramBot(TOKEN, { polling: { timeout: 1, interval: 100 } });
+
+/* Create your own API Token on https://steamapi.io */
+var STEAM_API_TOKEN = '';
+
+/* Create your own Steam API Token on https://steamcommunity.com/dev/apikey */
+var STEAM_OF_API_TOKEN = '';
+
+/* Steam Price Api URL */
+var STEAM_PRICE_API = 'https://api.steamapi.io/api/account?key={1}';
+
+/* Steam API URL's */
+var STEAM_SERVER_STATUS_API = 'https://api.steampowered.com/ICSGOServers_730/GetGameServersStatus/v1/?key={0}';
+var STEAM_USER_ID = 'http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key={0}&vanityurl={1}';
+
+//Load the request module
+var request = require('request')
+
+var opts = {
+    reply_markup: JSON.stringify(
+        {
+            force_reply: true
+        }
+    )
+};
+
+// Replacement like in Python
+if (!String.prototype.format) {
+    String.prototype.format = function () {
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, function (match, number) {
+            return typeof args[number] != 'undefined'
+                ? args[number]
+                : match
+                ;
         });
-        getData('https://api.exchangeratesapi.io/latest?base=EUR').then( data  => {  
-            const rate = data.rates;            
-            bot.sendMessage(chatId, ` EUR = ${rate[money].toFixed(2)}`);           
-        });       
+    };
 }
 
-// ** //
-// обработчик события присылания нам любого сообщения
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id; //получаем идентификатор диалога, чтобы отвечать именно тому пользователю, который нам что-то прислал
-    if(msg.text.toLowerCase() == '/menu'){
-        
-        // отправляем сообщение
-        bot.sendMessage(chatId, 'Меню:', { // прикрутим клаву
-            reply_markup: {
-                inline_keyboard: keyboard
-            }
-        });
-    }else if(msg.text.toLowerCase() == '/img'){
-        sendImg(chatId,'boy.jpg');
-    }else{
-        bot.sendMessage(chatId,'Я не понял')
+/* WELCOME MESSAGE */
+bot.on('message', function (msg) {
+    var chatId = msg.chat.id;
+    console.log(msg);
+    if (msg.text == "/start") {
+        // bot.sendMessage(chatId, "Hello, just tell me the name of CSGO Skin. Exempel: AWP | Asiimov (Field-Tested) Price will be automaticly updated in 1min");
+        bot.sendMessage(chatId, "Hello. Im CSGO Prices DevBot. I will help you to get some info about this game. Use /status to get servr status. Use /price to get Skin price. Or /help");
+    }
+    if (msg.text == "/help") {
+        // bot.sendMessage(chatId, "Hello, just tell me the name of CSGO Skin. Exempel: AWP | Asiimov (Field-Tested) Price will be automaticly updated in 1min");
+        bot.sendMessage(chatId, "/start /status /price /id");
     }
 });
 
-// ** //
-// обработчик событий нажатий на клавиатуру
-bot.on('callback_query', (query) => {
-    const chatId = query.message.chat.id;
-
-    if (query.data === 'RUB') { // курсы вылют
-            sendMoneyExchengeRates(chatId,'RUB');
-    }
-    
-    if (query.data === 'joke') { // погода            
-        getData('https://icanhazdadjoke.com/',{ method: 'GET', headers: {'Accept': 'application/json' }})
-        .then( data  => {   
-            console.log(data.joke)        
-        bot.sendMessage(chatId, ` ${data.joke}`);       
-        });       
-    }
-
-    if (query.data === 'wether') { // погода        
-        getData('https://api.openweathermap.org/data/2.5/weather?id=562450&appid=658790e853befcc579c25cc91de06623&units=metric')
-        .then( data  => {           
-        bot.sendMessage(chatId, ` ${data.name} : мин: ${data.main.temp_min}, макс: ${data.main.temp_max}`);       
-        });
-    }    
-
-    if (query.data === 'music') { // если музыка
-        bot.sendAudio(chatId,'./audio/1.mp3');        
-    }
-
-    if(query.data === 'img'){
-        getData('https://api.unsplash.com/photos/?client_id=0i3sh3ZkjbCnmFQFwPSFS_GF8m6tJokeEFfdmeWxgkw').then(
-            data  => { 
-                const pic = data[0].urls          
-               // console.log(pic.regular);
-                sendImg(chatId,pic.regular);       
+/* Get server status for CSGO */
+bot.on('message', function (msg) {
+    if (msg.text == "/status") {
+        var url = '';
+        url = STEAM_SERVER_STATUS_API.format(STEAM_OF_API_TOKEN);
+        request({
+            url: url,
+            json: true
+        }, function (error, response, data) {
+            if (!error && response.statusCode == 200) {
+                bot.sendMessage(msg.chat.id, "Server status: " + data['result']['matchmaking']['scheduler'] + ". Online Servers: " + data['result']['matchmaking']['online_servers'] + ". Online Players: " + data['result']['matchmaking']['online_players'] + ". Players Searching: " + data['result']['matchmaking']['searching_players'] + ".");
+            } else {
+                bot.sendMessage(msg.chat.id, "Sorry, I cant fetch data right now, try latter! :(")
             }
-        )
-        sendImg(chatId,'boy.jpg');       
-    };
+        })
+    }
+});
+
+
+var opts = {
+    reply_markup: JSON.stringify(
+        {
+            force_reply: true
+        }
+    )
+};
+
+/* Get name - return lowest price*/
+bot.on('message', function (msg) {
+    if (msg.text == '/price') {
+        bot.sendMessage(msg.chat.id, 'What Skin you are looking for?', opts)
+            .then(function (sended) {
+                bot.onReplyToMessage(msg.chat.id, sended.message_id, function (message) {
+                    var url = '';
+                    url = STEAM_PRICE_API.format(message.text, STEAM_API_TOKEN);
+                    /*console.log(url);*/
+                    request({
+                        url: url,
+                        json: true
+                    }, function (error, response, data) {
+                        if (!error && response.statusCode === 200) {
+                            bot.sendMessage(msg.chat.id, data['data']['market_hash_name'] + " - " + data['data']['prices']['lowest'] + "$");
+                        }
+                    })
+                });
+            });
+    }
+});
+
+/* Get stem User ID*/
+bot.on('message', function (msg) {
+    if (msg.text == '/id') {
+        bot.sendMessage(msg.chat.id, 'What is your custom ID? (https://steamcommunity.com/id/YOUR_ID)', opts)
+            .then(function (sended) {
+                bot.onReplyToMessage(msg.chat.id, sended.message_id, function (message) {
+                    var url = '';
+                    url = STEAM_USER_ID.format(STEAM_OF_API_TOKEN, message.text);
+                    // console.log(url);
+                    request({
+                        url: url,
+                        json: true
+                    }, function (error, response, data) {
+                        if (!error && response.statusCode === 200) {
+                            bot.sendMessage(msg.chat.id, "Your ID: " + data['response']['steamid']);
+                        }
+                    })
+                });
+            });
+    }
+});
+
+
+
+
+/* Dont Die BITCH! */
+process.on('uncaughtException', function (err) {
+    console.log('Caught exception: ' + err);
 });
