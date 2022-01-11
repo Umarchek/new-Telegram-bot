bd7bce933b41ef640ad1067124c7bf357033b0e3
UZBase <sharofiddinov.07@mail.ru>
2022-01-11 11:58:40 +0500

    added 1

---
+++ bot.js
@@ -0,0 +1,69 @@
+const Telegraf = require("telegraf");
+const bot = new Telegraf("5095484960:AAH-tVfHei20nF0On0UjCC5cm8cikWtTR5w");
+const mysql = require("mysql");
+
+const conn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "bot",
+});
+conn.connect(function (err) {
+  if (err) {
+    throw err;
+  }
+  console.log("connected !");
+  conn.query("SELECT * FROM fruit", function (err, result, fields) {
+    if (err) {
+      throw err;
+    }
+    dataStore = [];
+
+    result.forEach((item) => {
+      dataStore.push({
+        id: item.id,
+        name: item.name,
+        price: item.price,
+      });
+    });
+  });
+});
+
+const helpMessage = `
+    Для использования этого бота есть несколько команд:
+    /fruitlist - для просмотра списка фруктов.
+    /fruit <nama_buah> - чтобы увидеть цену на фрукты, основанную на названии фрукта.
+    `;
+bot.help((ctx) => {
+  ctx.reply(helpMessage);
+});
+
+bot.command("fruitlist", (ctx) => {
+  let fruitMessage = `Список фруктов : \n`;
+
+  dataStore.forEach((item) => {
+    fruitMessage += `${item.id}. ${item.name}\n`;
+  });
+  ctx.reply(fruitMessage);
+});
+
+bot.command("fruit", (ctx) => {
+  let input = ctx.message.text.split(" ");
+  if (input.length != 2) {
+    ctx.reply("Вы должны дать название фрукта в аргументе 2");
+    return;
+  }
+  //   console.log(input[1]);
+  let fruitInput = input[1];
+  dataStore.forEach((item) => {
+    if (item.name.includes(fruitInput)) {
+      let price = item.price.toLocaleString(undefined, {
+        minimumFractionDigits: 2,
+      });
+      ctx.reply("Rp " + price);
+      return;
+    }
+  });
+});
+
+bot.launch();
